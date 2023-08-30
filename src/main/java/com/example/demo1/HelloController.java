2a59db5934b89fcbe747329807560a249446aa57
Java Moth <javamoth@yahoo.com>
2023-08-30 06:07:21 -0600

    Made all numeric buttons functional, C button clears the display

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -17,4 +17,135 @@ public class HelloController {
         display.setText("Welcome to JavaFX Application!");
         button0.setText("!!!");
     }
+
+    @FXML
+    protected double onAddButtonClick(double number) {
+        return number;
+    }
+
+    @FXML
+    protected void onButton1Click() {
+        if (Double.parseDouble(display.getText()) == 0) {
+            display.setText("1");
+        }
+        else {
+            String num = display.getText();
+            num = num + "1";
+            display.setText(num);
+        }
+    }
+
+    @FXML
+    protected void onButton2Click() {
+        if (Double.parseDouble(display.getText()) == 0) {
+            display.setText("2");
+        }
+        else {
+            String num = display.getText();
+            num = num + "2";
+            display.setText(num);
+        }
+    }
+
+    @FXML
+    protected void onButton3Click() {
+        if (Double.parseDouble(display.getText()) == 0) {
+            display.setText("3");
+        }
+        else {
+            String num = display.getText();
+            num = num + "3";
+            display.setText(num);
+        }
+    }
+
+    @FXML
+    protected void onButton4Click() {
+        if (Double.parseDouble(display.getText()) == 0) {
+            display.setText("4");
+        }
+        else {
+            String num = display.getText();
+            num = num + "4";
+            display.setText(num);
+        }
+    }
+
+    @FXML
+    protected void onButton5Click() {
+        if (Double.parseDouble(display.getText()) == 0) {
+            display.setText("5");
+        }
+        else {
+            String num = display.getText();
+            num = num + "5";
+            display.setText(num);
+        }
+    }
+
+    @FXML
+    protected void onButton6Click() {
+        if (Double.parseDouble(display.getText()) == 0) {
+            display.setText("6");
+        }
+        else {
+            String num = display.getText();
+            num = num + "6";
+            display.setText(num);
+        }
+    }
+
+    @FXML
+    protected void onButton7Click() {
+        if (Double.parseDouble(display.getText()) == 0) {
+            display.setText("7");
+        }
+        else {
+            String num = display.getText();
+            num = num + "7";
+            display.setText(num);
+        }
+    }
+
+    @FXML
+    protected void onButton8Click() {
+        if (Double.parseDouble(display.getText()) == 0) {
+            display.setText("8");
+        }
+        else {
+            String num = display.getText();
+            num = num + "8";
+            display.setText(num);
+        }
+    }
+
+    @FXML
+    protected void onButton9Click() {
+        if (Double.parseDouble(display.getText()) == 0) {
+            display.setText("9");
+        }
+        else {
+            String num = display.getText();
+            num = num + "9";
+            display.setText(num);
+        }
+    }
+
+    @FXML
+    protected void onButton0Click() {
+        if (Double.parseDouble(display.getText()) == 0) {
+            display.setText("0");
+        }
+        else {
+            String num = display.getText();
+            num = num + "0";
+            display.setText(num);
+        }
+    }
+
+
+    @FXML
+    protected void onButtonCClick() {
+        display.setText("0");
+    }
 }
