1841d910f5d74b5ffb56a3fd9723c2752e2fb88e
Java Moth <javamoth@yahoo.com>
2023-08-30 05:29:11 -0600

    Defined buttons in the controller class

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -1,14 +1,20 @@
 package com.example.demo1;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label display;
+    @FXML
+    private Button button0, button1, button2, button3, button4, button5,
+            button6, button7, button8, button9, buttonAdd, buttonSubtract,
+            buttonDivide, buttonMultiply, buttonEquals, buttonC;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        display.setText("Welcome to JavaFX Application!");
+        button0.setText("!!!");
     }
 }
